ff5ca5678dca55b23f407eac9147806a1ee8b2f5
Jorge Cotillo <jorge.cotillo@cotillo-corp.com>
2019-03-04 09:01:51 -0800

    modified bash file to retrieve a storage account name

--- scripts/palo-alto/create-bootstrap.sh
+++ scripts/palo-alto/create-bootstrap.sh
@@ -1,10 +1,15 @@
+STORAGE_RESOURCE_GROUP_NAME=$1
+SUBSCRIPTION_ID=$2
 INGRESS_BLOB_URI=$1
 INGRESS_FILE_SHARE_NAME=$2
 EGRESS_BLOB_URI=$1
 EGRESS_FILE_SHARE_NAME=$2
 
-wget -O igress.xml $INGRESS_BLOB_URI
-wget -O egress.xml $EGRESS_BLOB_URI
+
+palo_alto_storage_account_name="$(az.cmd storage account list -g "${STORAGE_RESOURCE_GROUP_NAME}" --subscription ${SUBSCRIPTION_ID} --query [].name --output tsv)"
+echo $palo_alto_storage_account_name
+
+access_key="$()"
 
 az storage file upload --share-name $INGRESS_FILE_SHARE_NAME --source ingress.xml
 az storage file upload --share-name $EGRESS_FILE_SHARE_NAME --source egress.xml
