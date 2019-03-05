3ce0cceb491c140499193f873f0333c3089df8d9
Jorge Cotillo <jorge.cotillo@gmail.com>
2019-03-04 16:16:34 -0800

    updated storage template to perform copy loop, updated file share upload script

--- scripts/palo-alto/create-bootstrap.sh
+++ scripts/palo-alto/create-bootstrap.sh
@@ -1,16 +1,14 @@
 STORAGE_RESOURCE_GROUP_NAME=$1
 SUBSCRIPTION_ID=$2
-INGRESS_BLOB_URI=$1
-INGRESS_FILE_SHARE_NAME=$2
-EGRESS_BLOB_URI=$1
-EGRESS_FILE_SHARE_NAME=$2
+INGRESS_FILE_SHARE_NAME=$3
+INGRESS_FILE_PATH=$4
+EGRESS_FILE_SHARE_NAME=$5
+EGRESS_FILE_PATH=$6
 
 
-palo_alto_storage_account_name="$(az.cmd storage account list -g "${STORAGE_RESOURCE_GROUP_NAME}" --subscription ${SUBSCRIPTION_ID} --query [].name --output tsv)"
-echo $palo_alto_storage_account_name
+palo_alto_storage_account_name="$(az storage account list -g "${STORAGE_RESOURCE_GROUP_NAME}" --subscription ${SUBSCRIPTION_ID} --query [].name --output tsv)"
+palo_alto_storage_account_key="$(az storage account keys list -g "${STORAGE_RESOURCE_GROUP_NAME}" --subscription ${SUBSCRIPTION_ID} --account-name ${palo_alto_storage_account_name} --query [0].value --output tsv)"
 
-access_key="$()"
-
-az storage file upload --share-name $INGRESS_FILE_SHARE_NAME --source ingress.xml
-az storage file upload --share-name $EGRESS_FILE_SHARE_NAME --source egress.xml
+az storage file upload --account-key $palo_alto_storage_account_key --account-name $palo_alto_storage_account_name --share-name $INGRESS_FILE_SHARE_NAME --source $INGRESS_FILE_PATH
+az storage file upload --account-key $palo_alto_storage_account_key --account-name $palo_alto_storage_account_name --share-name $EGRESS_FILE_SHARE_NAME --source $EGRESS_FILE_PATH
 
